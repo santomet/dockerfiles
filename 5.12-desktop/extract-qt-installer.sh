2dcf994635f3e8049dcc4897243c712e2026c5ad
Marek Šanta <teslan223@gmail.com>
2020-03-13 18:06:43 +0100

    revert back to use env variables

--- 5.12-desktop/extract-qt-installer.sh
+++ 5.12-desktop/extract-qt-installer.sh
@@ -156,8 +156,8 @@ Controller.prototype.WelcomePageCallback = function() {
 }
 Controller.prototype.CredentialsPageCallback = function() {
 	log("Login Page");
-	var login = "teslanqtci@dopi.ci";
-	var password = "ThisPassIs123Dummy";
+	var login = installer.environmentVariable("QT_CI_LOGIN");
+	var password = installer.environmentVariable("QT_CI_PASSWORD");
 	if (login === "" || password === "") {
         log("Login or password not detected");
 		gui.clickButton(buttons.CommitButton);
